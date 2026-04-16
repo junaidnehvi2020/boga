589f1eff13a2088ea13c295e24af74366bdeb9c9
Junaid Nehvi <junaid.nehvi@gmail.com>
2026-04-16 22:45:57 +0300

    Fix metro.config.js ES module with proper __dirname polyfill

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,9 @@
-import { getDefaultConfig } from 'expo/metro-config';
+import { getDefaultConfig } from '@expo/metro-config';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config = getDefaultConfig(__dirname);
 
